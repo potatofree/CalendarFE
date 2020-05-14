0a13133d83d217232ea3bcf95c94786e23159b28
sashkavolkov <sashka.volkov@gmail.com>
2020-05-14 16:40:45 +0200

    Task from json reading and displaing.

--- src/index.js
+++ src/index.js
@@ -13,14 +13,11 @@ function NumberCell(props) {
 class MonthSection extends React.Component {
 
   render () {
-    const day = this.props.day;
-    const month = this.props.month;
-    const year = this.props.year;
 
     return (
-    <div class="month-section">
-      <MonthNav day={day} month={month} year={year} onChange={(date) => this.props.onChange(date)}/>
-      <MonthView day={day} month={month} year={year} onChange = {(date) => this.props.onChange(date)} />
+    <div className="month-section">
+      <MonthNav date={this.props.date} onChange={(date) => this.props.onChange(date)}/>
+      <MonthView date={this.props.date} onChange = {(date) => this.props.onChange(date)} />
     </div>
       );}
 }
@@ -28,8 +25,8 @@ class MonthSection extends React.Component {
 class MonthNav extends React.Component {
 
   handleClick(direction) {
-    const year = this.props.year;
-    const month = this.props.month;
+    const year = this.props.date.year;
+    const month = this.props.date.month;
 
     if (direction === 'back'){
       if(month === 1) {
@@ -70,7 +67,7 @@ class MonthNav extends React.Component {
     return (
     <div className="month-name">
       <button className="month-back-arrow" onClick={() => this.handleClick('back')}>prev</button>
-      {monthList[this.props.month-1]}
+      {monthList[this.props.date.month-1]}
       <button className="month-forward-arrow" onClick={() => this.handleClick('forward')}>next</button>
     </div>
     );}
@@ -101,14 +98,14 @@ class MonthView extends React.Component {
     } else {
     this.props.onChange({
       day: number,
-      month: this.props.month,
-      year: this.props.year,
+      month: this.props.date.month,
+      year: this.props.date.year,
     });}
   }
 
   render() {
     const weekList =['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-    const month = this.monthArray(this.props.month, this.props.year);
+    const month = this.monthArray(this.props.date.month, this.props.date.year);
     return (
       <div className="month">
         {weekList.map(dayName => <span className="dayname">{dayName}</span>)}
@@ -130,7 +127,7 @@ class Today extends React.Component {
   }
 
   render() {
-    const todayText = `${this.props.day} - ${this.props.month} - ${this.props.year}`;
+    const todayText = `${this.props.date.day} - ${this.props.date.month} - ${this.props.date.year}`;
     return(
       <div className="date">
         <h2 className="today"><span onClick={() => this.backToday()}>Today</span> is {todayText}</h2>
@@ -138,94 +135,187 @@ class Today extends React.Component {
     );
   }
 }
+class PlannerSection extends React.Component {
 
-class Calender extends React.Component {
-
-  constructor(props) {
-    super(props);
-    const today = new Date();
-    this.state = {
-      day: today.getDate(),
-      month: today.getMonth() + 1,
-      year: today.getFullYear(),
+  render()
+  {
+    const times = {
+      start: 7,
+      end: 22,
     };
-  }
-
-  handleDateChange(date) {
-    this.setState({
-      day: date.day,
-      month: date.month,
-      year: date.year,
-    });
-  }
-
-  render() {
     return (
-      <div className="container">
-        <header>
-          <h1 className="hname">Just another calender</h1>
-        </header>
-        <MonthSection
-          day={this.state.day}
-          month={this.state.month}
-          year={this.state.year}
-          onChange={(date) => this.handleDateChange(date)}
-        />
-        <Today
-          day={this.state.day}
-          month={this.state.month}
-          year={this.state.year}
-          onChange={(date) => this.handleDateChange(date)}
-        />
-        <div className="day calender">
-          <span className="hour">7:00</span>
-          <span className="hour">8:00</span>
-          <span className="hour">9:00</span>
-          <span className="hour">10:00</span>
-          <span className="hour">11:00</span>
-          <span className="hour">12:00</span>
-          <span className="hour">13:00</span>
-          <span className="hour">14:00</span>
-          <span className="hour">15:00</span>
-          <span className="hour">16:00</span>
-          <span className="hour">17:00</span>
-          <span className="hour">18:00</span>
-          <span className="hour">19:00</span>
-          <span className="hour">20:00</span>
-          <span className="hour">21:00</span>
-          <span className="hour">22:00</span>
-          <span className="task A">
-            <span className="DC">
-              <h3>Just make this site.</h3>
-              <p>Calm down<br/>and<br/>Justify!</p>
-            </span>
-          </span>
-          <div className="task B">
-            <span className="DC">Just commit it in the git.</span>
-          </div>
-          <span className="task C">
-            <span className="DC">Just have a rest.</span>
-          </span>
-
-
-        </div>
-        <footer>
-          <h5>Just some info in a bottom.</h5>
-          <div id="like_button_container"></div>
-        </footer>
+      <div className="day calender">
+        <PlannerView times={times}/>
+        <Tasks times={times} date={this.props.date} taskList={this.props.taskList}/>
       </div>
-
     );
   }
-}
+  }
+
+  function PlannerView(props) {
+    const start = props.times.start;
+    const end = props.times.end;
+    let hours = [];
+
+    for (let i=start; i<=end; i++){
+      hours.push(i);
+      }
+    return (
+      hours.map(hour => {return(<span className="hour">{hour<10 ? `0${hour}:00` : `${hour}:00`}</span>)})
+      );
+  }
+
+  function isDateEqual(date1, date2) {
+    if (date1.day !== date2.day || date1.month !== date2.month || date1.year !== date2.year) {
+      return false;
+    }
+    return true;
+  }
+
+  class Tasks extends React.Component {
+            render() {
+              const taskList = this.props.taskList;
+              const date = this.props.date;
+              return (
+                taskList.map( task => {
+                  return(isDateEqual(task.date, date) ? <Task task={task} times={this.props.times}/> : null)})
+              );
+            }
+          }
+  function Task(props) {
+    const task = props.task;
+            const dayStart = props.times.start;
+            const dayEnd = props.times.end;
+            let firstRow = 0;
+            let lastRow = 0;
+            console.log('task');
+            if (task.time.end <= dayStart || task.time.start > dayEnd ) {
+              console.log(task.time);
+              return null;
+            } else {
+              if (task.time.start<=dayStart) {
+                firstRow = 1;
+                console.log(firstRow);
+              } else {
+                firstRow = task.time.start - dayStart + 1;
+                console.log(firstRow);
+              }
+              if (task.time.end > dayEnd) {
+                lastRow = dayEnd - dayStart + 1;
+                console.log(lastRow);
+              } else {
+                lastRow = task.time.end - dayStart + 1;
+                console.log(lastRow);
+              }
+            }
+            const style = {
+          gridRow: `${firstRow} / ${lastRow}`
+            }
+            console.log(`${firstRow} / ${lastRow}`);
+            console.log(style);
+            return(
+              <div className={`task ${task.id}`} style={style}>
+                <span className="DC">
+                  <h3>{task.name}</h3>
+                  <p>{task.task}</p>
+                </span>
+              </div>
+            )
+          }
+
+          class Calender extends React.Component {
+
+            constructor(props) {
+              super(props);
+              const today = new Date();
+              this.state = {
+                date: {
+                  day: today.getDate(),
+                  month: today.getMonth() + 1,
+                  year: today.getFullYear(),
+                }
+              };
+            }
+
+            handleDateChange(date) {
+              this.setState({date: date});
+            }
+
+            render() {
+              return (
+                <div className="container">
+                  <header>
+                    <h1 className="hname">Just another calender</h1>
+                  </header>
+                  <MonthSection
+                    date={this.state.date}
+                    onChange={(date) => this.handleDateChange(date)}
+                  />
+                  <Today
+                    date={this.state.date}
+                    onChange={(date) => this.handleDateChange(date)}
+                  />
+                  <PlannerSection taskList={this.props.taskList} date={this.state.date}/>
+                  <footer>
+                    <h5>Just some info in a bottom.</h5>
+                  </footer>
+                </div>
+
+              );
+            }
+          }
+
+  const TASKS = [
+    {
+            id: 1,
+            date: {
+              day: 13,
+              month: 5,
+              year: 2020,
+            },
+            time: {
+              start: 7,
+              end: 10,
+            },
+          name: `Just make this site.`,
+          task: `Calm down and Justify!`,
+        },
+  {
+            id: 2,
+            date: {
+              day: 13,
+              month: 5,
+              year: 2020,
+            },
+            time: {
+      start: 13,
+      end: 15,
+    },
+    name: `Lunch`,
+    task: `Just eat smthng.`,
+    },
+{
+  id: 3,
+  date: {
+    day: 14,
+    month: 5,
+    year: 2020,
+  },
+  time: {
+    start: 18,
+    end: 23,
+  },
+  name: `Git-git.`,
+  task: `Keep calm and just commit.`,
+},
+];
 
 ReactDOM.render(
   <React.StrictMode>
-    <Calender />
+    <Calender taskList={TASKS}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
